a57327ba3b370bb2c80b4d3334f231ddd0f7d58b
David França <davidemanuel.cf@gmail.com>
2020-09-22 17:08:18 -0300

    Estilo inicial

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+
+export default function Login() {
+  return (
+    <div id="page-login">
+      <div id="page-login-contend" className="container">
+        <h1 className="title">Ideal store for your jewelry shopping</h1>
+        <hr />
+      </div>
+    </div>
+  );
+}
