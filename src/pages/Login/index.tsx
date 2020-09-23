8d2450f6a82aa0dcf08e738f9278eecb6baaade1
David França <davidemanuel.cf@gmail.com>
2020-09-23 17:39:28 -0300

    Fim do Login Page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,7 +6,12 @@ export default function Login() {
     <div id="page-login">
       <div id="page-login-contend" className="container">
         <h1 className="title">Ideal store for your jewelry shopping</h1>
-        <hr />
+        <hr className="rounded" />
+        <div className="buttons-container">
+          <button>SIGN UP WITH EMAIL</button>
+          <button className="facebook">SIGN UP WITH FACEBOOK</button>
+        </div>
+        <hr className="rounded" />
       </div>
     </div>
   );
